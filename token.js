feea57f3fa89c23c0be52ae28f973856f4f9a33e
alfredlog <alfredmunganga@icloud.com>
2025-02-05 20:59:32 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,10 +1,4 @@
-function h(){
-    var token = document.getElementById("token1").value
-    sessionStorage.setItem("t1", token)
-}
-var t1 = sessionStorage.getItem("t1")
-console.log(t1)
-var T1 = document.getElementById("token4").value = t1
+
 function z(){
     var token = document.getElementById("token2").value
     sessionStorage.setItem("t2", token)
